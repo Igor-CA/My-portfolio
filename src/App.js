6ea7c197bba20d27be322814c57394c586ebb8c8
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-09-03 22:11:19 -0300

    Corrections for deployment

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
 	return (
 		<div className="App">
-			<BrowserRouter>
+			<BrowserRouter basename="/My-portfolio">
 				<NavBar onMobile={onMobile}></NavBar>
 
 				<Routes>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,6 @@ import {
 	faList,
 	faXmark,
 } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 import { useState } from "react";
 export default function NavBar({ onMobile }) {
