a8c45517ff5db171c061e754599e29540ee38d38
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-11-03 23:46:49 -0300

    Added new skills

--- src/components/Habilidade.jsx
+++ src/components/Habilidade.jsx
@@ -1,7 +1,6 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import "./Habilidade.css"
-export default function Habilidade({icon, name, description }) {
-    console.log(description)
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Habilidade.css";
+export default function Habilidade({ icon, name, description, customIcon }) {
 	return (
 		<li className="habilidades__item">
 			<div>
@@ -9,7 +8,24 @@ export default function Habilidade({icon, name, description }) {
 				<p>{description}</p>
 			</div>
 			<div>
-				<FontAwesomeIcon fixedWidth icon={icon} className='habilidades__icon'/>
+				{icon ? (
+					<FontAwesomeIcon
+						fixedWidth
+						icon={icon}
+						className="habilidades__icon"
+					/>
+				) : (
+					<svg
+						aria-hidden="true"
+						focusable="false"
+						width="80px"
+						height="80px"
+						viewBox="0 0 80 80"
+						className="svg-inline--fa fa-fw habilidades__icon habilidades__icon"
+					>
+						<path fill="currentColor" d={customIcon} />
+					</svg>
+				)}
 			</div>
 		</li>
 	);
